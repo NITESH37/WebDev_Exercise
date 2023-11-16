8e51fd86ff278e1cc30d4e04e189b88eec9b0000
Nitesh <niteshmuz06@gmail.com>
2023-11-16 11:38:57 +0530

    fund transfer

---
+++ Personal Projects/FundTransfer/resources/views/welcome.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <div class="m-5 bg-lime-500">
+        <form action="">
+            <div class="space-y-12 bg-red-700">
+                <div class="flex items-center mt-4">
+                    <h1>Funnd Transfer</h1>
+                </div>
+                <div class="mt-6 flex items-center justify-end gap-x-6">
+                    <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
+                    <button type="submit"
+                        class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</body>
+
+</html>
