ac927d25c222f46ec35a71b7df276e288da2e60b
Nitesh <niteshmuz06@gmail.com>
2023-11-23 12:39:11 +0530

    some changes in add and subtract

--- Tutorials/React js/03counter/src/App.jsx
+++ Tutorials/React js/03counter/src/App.jsx
@@ -5,22 +5,24 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [counter, setCounter] = useState(0);
 
   const addValue = () => {
     setCount(count + 1)
   }
 
   const removeValue = () =>{
-    setCount(count -1)
+    setCounter(count -1)
   }
 
   return (
     <>
       <h1>Counter</h1>
-      <h3>Value is here: {count} </h3>
+      <h3>Add Value is here: {count} </h3>
+      <h3>Subtract Value is here: {counter} </h3>
       <button onClick={addValue}>Add Value: {count}</button>
       <br />
-      <button onClick={removeValue}>Subtract Value: {count}</button>
+      <button onClick={removeValue}>Subtract Value: {counter}</button>
     </>
   );
 }
