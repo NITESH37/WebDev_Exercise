d5b59703ad1279df3b8950c55608df164b638671
Nitesh <niteshkumarpandit37@gmail.com>
2023-12-03 14:31:21 +0530

    added routing and heder section

--- Personal Projects/React Js/securityGaurd/src/components/Header/Header.jsx
+++ Personal Projects/React Js/securityGaurd/src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <header className="bg-slate-500 shadow sticky top-0">
       <nav className="bg-gray-600 border-green-400 px-4 lg:px-6 py-2">
-        <div>
+        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
           <Link to="/" className="flex items-center">
             <img
               src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
@@ -29,6 +29,42 @@ export default function Header() {
                 }
               >
                 Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/about"
+                className={({ isActive }) =>
+                  `block py-2 pr-4 pl-3 duration-200 ${
+                    isActive ? "text-orange-700" : "text-gray-700"
+                  } border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                }
+              >
+                About
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/services"
+                className={({ isActive }) =>
+                  `block py-2 pr-4 pl-3 duration-200 ${
+                    isActive ? "text-orange-700" : "text-gray-700"
+                  } border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                }
+              >
+                Services
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/contacts"
+                className={({ isActive }) =>
+                  `block py-2 pr-4 pl-3 duration-200 ${
+                    isActive ? "text-orange-700" : "text-gray-700"
+                  } border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                }
+              >
+                Contacts
               </NavLink>
             </li>
           </ul>
--- Personal Projects/React Js/securityGaurd/src/main.jsx
+++ Personal Projects/React Js/securityGaurd/src/main.jsx
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
-
+import Contact from './components/Contact/Contact.jsx'
+import About from './components/About/About.jsx'
+import Services from './components/Serrvices/Services.jsx'
 
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={ <App />} />
-    
+    <Route path='/' element={ <App />} >
+    <Route path='contacts' element= { <Contact /> } />
+    <Route path='about' element={ <About />} />
+    <Route path='services' element={ <Services />} />
+    </Route>
   )
 )
 
