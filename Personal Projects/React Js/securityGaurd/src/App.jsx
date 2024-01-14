cb137193602f5b9dc4d26ef6c3bc608fcf74ccff
Nitesh <niteshkumarpandit37@gmail.com>
2024-01-14 07:56:25 +0530

    add

--- Personal Projects/React Js/securityGaurd/src/App.jsx
+++ Personal Projects/React Js/securityGaurd/src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
    <main className="relative">
     <Nav />
-    <section className="sm:pl-16 pl-8 sm:pb-24 pb-12">
+    <section className="sm:pl-16 pl-8 sm:pr-16 pr-8 sm:pb-24 pb-12">
       <Hero />
     </section>
    </main>
--- Personal Projects/React Js/securityGaurd/src/assests/images/index.js
+++ Personal Projects/React Js/securityGaurd/src/assests/images/index.js
@@ -1,4 +1,6 @@
 import headerLogo from './header-logo.svg'
+import heroBg from './hero-bg.jpg';
 export {
-    headerLogo
+    headerLogo,
+    heroBg
 }
