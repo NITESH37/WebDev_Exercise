9eeb98e9764eade1e92ea276566c6439b9575052
Nitesh <niteshkumarpandit37@gmail.com>
2024-08-21 00:10:38 +0530

    add and sbstract two number

---
+++ Tutorials/LNMC Assigenment/C Programming/minus.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main()
+{
+    int a, b, minus;
+    printf("Enter first number: ");
+    scanf("%d", &a);
+    printf("Enter second number: ");
+    scanf("%d", &b);
+    minus = a - b;
+    printf("minus of two number is %d ", minus);
+}
