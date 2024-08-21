e8a5d891cd79dbbe697986836e85037e73df6d93
Nitesh <niteshkumarpandit37@gmail.com>
2024-08-21 14:08:41 +0530

    find percentage of marks

---
+++ Tutorials/LNMC Assigenment/C Programming/percentagemarks.c
@@ -0,0 +1,18 @@
+#include <stdio.h>>
+
+void main()
+{
+    int sub1, sub2, sub3;
+    float avg, per, total;
+    printf("Enter first subject marks: ");
+    scanf("%d", &sub1);
+    printf("Enter second subject marks: ");
+    scanf("%d", &sub2);
+    printf("Enter third subject marks: ");
+    scanf("%d", &sub3);
+    total = sub1 + sub2 + sub3;
+    per = (total * 100) / 300;
+    avg = total / 300;
+    printf("Average of marks = %.2f \n", avg);
+    printf("Percantage of marks = %.2f", per);
+}
